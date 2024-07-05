eef8c8303908a92afbe2b6aa68944cceeb852aee
igordon215 <igordon215@gmail.com>
2024-07-05 15:45:50 -0400

    working on lab

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,34 +3,54 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String s = "";
+        for (int i = start; i < stop; i = i + 1) {
+            if (isEven(i)) {
+                s = s + Integer.toString(i);
+            }
+        }
+        return s;
     }
 
-
-    public static String getOddNumbers(int start, int stop) {
-        return null;
+    // (i % 2 == 0) if that is true i is even!
+    static boolean isEven(int i) {
+        return (i % 2 == 0);
     }
+        public static String getOddNumbers(int start, int stop) {
+            String s = "";
+            for (int i = start; i < stop; i = i + 1) {
+                if (!isEven(i)) {
+                    s = s + Integer.toString(i);
+                }
+            }
+            return s;
+        }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        return getExponentiations(start, stop, step, 2);
     }
 
     public static String getRange(int start) {
-        return null;
+        return getExponentiations(0, stop, 1, 1);
     }
 
+
     public static String getRange(int start, int stop) {
-        return null;
+        return getExponentiations(start, stop, 1, 1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        return getExponentiations(start, stop, step, 1);
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String s = "";
+        for (int i = start; i < stop; i = i + step) {
+            s = s + Integer.toString((int) Math.pow(i, exponent));
+        }
+        return s;
     }
 }
